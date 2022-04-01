abc54c7ebce267d86768c9fd587f69e9a08dffb5
six-two <git@six-two.dev>
2022-04-01 23:33:00 +0200

    Added header and footer output to replay.py

--- src/shell_command_logger/replay.py
+++ src/shell_command_logger/replay.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import argparse
+import json
+import shlex
 import subprocess
 import sys
+import traceback
+from typing import Optional
+# pip dependency
+from termcolor import cprint
+
+EXTENSIONS = [".json", ".log", ".time"]
 
 
 def replay_command(output_file: str) -> int:
+    metadata = parse_metadata(f"{output_file}.json")
+    
+    metadata and print_header(metadata)
+
     script_command = [
         "scriptreplay",
         "--log-out", f"{output_file}.log", # read the output file
@@ -14,9 +26,53 @@ def replay_command(output_file: str) -> int:
 
     try:
         exit_code = subprocess.call(script_command)
+
+        metadata and print_footer(metadata)
         return exit_code
     except KeyboardInterrupt:
         return 2
+
+
+def parse_metadata(path: str) -> Optional[dict]:
+    try:
+        with open(path) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        cprint(f"[scl] Metadata file does not exist: '{path}'", "red", attrs=["bold"])
+    except Exception as e:
+        cprint(f"[scl] Internal error while parsing the metadata in '{path}'", "red", attrs=["bold"])
+        traceback.print_exc()
+    return None
+
+
+def print_header(metadata: dict) -> None:
+    command = shlex.join(metadata["command"])
+    user = metadata["user"]
+    hostname = metadata["hostname"]
+    start_time = metadata["start_time"]
+
+    cprint(f"[scl] Command executed by {user}@{hostname} at {start_time}", "blue", attrs=["bold"])
+    cprint(f"[scl] Command: {command}", "blue", attrs=["bold"])
+
+
+def print_footer(metadata: dict) -> None:
+    end_time = metadata["end_time"]
+    status_code = metadata["status_code"]
+    error_message = metadata["error_message"]
+
+    if status_code == -1:
+        cprint(f"[scl] Exited at {end_time} because of internal error", "red", attrs=["bold"])
+        cprint(f"[scl] Error message: {error_message}", "red", attrs=["bold"])
+    else:
+        color = "green" if status_code == 0 else "red"
+        cprint(f"[scl] Exited at {end_time} with code {status_code}", color, attrs=["bold"])
+
+
+def remove_extension(path: str) -> str:
+    for extension in EXTENSIONS:
+        if path.endswith(extension):
+            return path[:-len(extension)]
+    return path
 
 
 def main_replay(arguments: list[str]) -> int:
@@ -26,11 +82,7 @@ def main_replay(arguments: list[str]) -> int:
 
     # Allow specifying the basename (like ~/.shell-command-logs/echo/2022w11g_133650_63ff),
     # or either file (the *.log or the *.time). If a file with the extention is given, the extension is removed
-    path = str(args.path)
-    if path.endswith(".log"):
-        path = path[:-4]
-    elif path.endswith(".time"):
-        path = path[:-5]
+    path = remove_extension(args.path)
 
     exit_code = replay_command(path)
     return exit_code
